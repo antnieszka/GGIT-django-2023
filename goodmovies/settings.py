c69074b7be17361b69e3df8bc258fb52982565bd
antnieszka <5757700+antnieszka@users.noreply.github.com>
2023-05-27 15:44:07 +0200

    Widoki użytkownikow z ikonami

--- goodmovies/settings.py
+++ goodmovies/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "movies",  # nasza aplikacja
+    "movies",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pl"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Warsaw"
 
 USE_I18N = True
 
--- movies/views.py
+++ movies/views.py
@@ -1,23 +1,12 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import datetime  # NOWE
-
-# Create your views here.
-# def hello_world(request):
-#     return HttpResponse("Witaj świecie!")
-
-# Create your views here.
-# def hello_world(request):
-#     return render(request, template_name="hello.html")  # NOWE
-
-
-# Create your views here.
-def hello_world(request):
-    our_context = {"time": datetime.now()}  # NOWE
-    return render(request, template_name="hello.html", context=our_context)  # NOWE
-
+from datetime import datetime
 
 from movies.models import Movie
+
+
+def hello_world(request):
+    our_context = {"time": datetime.now()}
+    return render(request, template_name="hello.html", context=our_context)
 
 
 def list_movies(request):
