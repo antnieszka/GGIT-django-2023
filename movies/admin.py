872b7228ae874c49e76e9f6bace343cf8d12dfed
antnieszka <5757700+antnieszka@users.noreply.github.com>
2023-06-03 11:19:58 +0200

    Model reżysera

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from movies.models import Movie
+from movies.models import Movie, Director
 
 admin.site.register(Movie)
+admin.site.register(Director)
--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,30 @@ class Movie(models.Model):
     short_description = models.TextField()
     published_at = models.DateTimeField()
     rating = models.IntegerField()
+    director = models.ForeignKey(
+        to="movies.Director",
+        on_delete=models.CASCADE,
+        verbose_name="reżyser",
+        related_name="movies",
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "film"
+        verbose_name_plural = "filmy"
 
     def __str__(self):
         return self.title
+
+
+class Director(models.Model):
+    first_name = models.CharField(verbose_name="imię", max_length=100)
+    last_name = models.CharField(verbose_name="nazwisko", max_length=100)
+    about = models.TextField(verbose_name="opis", blank=True)
+
+    class Meta:
+        verbose_name = "reżyser"
+        verbose_name_plural = "reżyserzy"
+
+    def __str__(self):
+        return "Reżyser: " + self.first_name + " " + self.last_name
